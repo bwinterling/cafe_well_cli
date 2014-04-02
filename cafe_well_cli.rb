a8dc351c782728f8203cff59f971076ff37e72f3
Brian Winterling <bwinterling@itriagehealth.com>
2014-04-02 14:51:38 -0600

    Initial set up

---
+++ cafe_well_cli.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+require 'mechanize'
+require 'thor'
+
+class CafeWellCLI < Thor
+
+  desc "update_move_to_improve DATE ACTIVITY MINUTES", "updates cafe well with activity activity details"
+  long_desc <<-LONGDESC
+    >$ cafe_well_cli.rb update_move_to_improve "04/31/2014" "Walking" "30"
+
+    will update Cafe Well with the entered activity.
+
+  LONGDESC
+
+  def update_move_to_improve(date, activity, minutes)
+    puts "#{ENV["CAFEWELL_USER"]} did a little #{activity} for #{minutes} mins on #{date}."
+    #logged in?
+      # no
+        # log in
+      # yes
+        # enter date (enter value or use date picker)
+        # enter activity (select list)
+        # enter minutes (value)
+        # click 'Report'
+  end
+
+end
+
+CafeWellCLI.start()
---
+++ cafe_well_cli_test.rb
@@ -0,0 +1,12 @@
+gem 'minitest', '~> 4.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative 'cafe_well_cli'
+
+class CafeWellCLITest < MiniTest::Test
+
+  def test_update_move_to_improve
+
+  end
+
+end
